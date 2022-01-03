050c930e12511afd5001afbc2470532bdd0acffd
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2022-01-02 22:51:27 -0300

    Created at BeerControllerTest Test DELETEComIdInvalidoStatusNotFoundRetornado

--- src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
+++ src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
@@ -166,6 +166,17 @@ public class BeerControllerTest {
                 .andExpect(status().isNoContent());
     }
 
+    @Test
+    void quandoChamadoDELETEComIdInvalidoEntaoNotFoundStatusERetornado() throws Exception {
+        //when
+        doThrow(BeerNotFoundException.class).when(beerService).deleteById(INVALID_BEER_ID);
+
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.delete(BEER_API_URL_PATH + "/" + INVALID_BEER_ID)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
     //    @Test
 //    void whenPATCHIsCalledToIncrementGreatherThanMaxThenBadRequestStatusIsReturned() throws Exception {
 //        QuantityDTO quantityDTO = QuantityDTO.builder()
